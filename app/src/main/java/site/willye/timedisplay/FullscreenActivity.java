0bc1239aa12b4b31e2650c1bf81ea380bbf5f71a
will ye <williamyeny@gmail.com>
2017-07-14 23:20:55 -0400

    fullscreen when onresume

--- app/src/main/java/site/willye/timedisplay/FullscreenActivity.java
+++ app/src/main/java/site/willye/timedisplay/FullscreenActivity.java
@@ -38,15 +38,15 @@ public class FullscreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_fullscreen);
 
+    }
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+
         View decorView = getWindow().getDecorView();
-// Hide both the navigation bar and the status bar.
-// SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-// a general rule, you should design your app to hide the status bar whenever you
-// hide the navigation bar.
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
         decorView.setSystemUiVisibility(uiOptions);
 
     }
-
 
 }
